142f4b45cb3613952ee19d7bfbd16b08adf25acf
cr4t3 <devcrateloper+github@gmail.com>
2024-02-06 13:11:38 -0300

    Changed version to 0.1.0

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import argparse, parser
 aparser = argparse.ArgumentParser()
 
 aparser.add_argument("-f", "--file")
-aparser.add_argument("-v", "--version", action="version", version="CrateAssembly 0.0.1")
+aparser.add_argument("-v", "--version", action="version", version="CrateAssembly 0.1.0")
 
 args = aparser.parse_args()
 
