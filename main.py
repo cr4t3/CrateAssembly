a72843d3df7bfed3c87e8d9241fda2f9fe76b5c1
cr4t3 <devcrateloper+github@gmail.com>
2024-04-24 22:42:10 -0300

    Updated version

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import argparse, interpreter
 
 aparser = argparse.ArgumentParser()
 
-version = "CrateAssembly 0.1.0"
+unstable = True
+
+version = "CrateAssembly 0.2.0" + " (Unstable)" if unstable else ""
 
 aparser.add_argument("file", help="Files path")
 aparser.add_argument("-v", "--version", action="version", version=version)
